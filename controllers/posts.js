71b30f9b82ddbc67e7eb8fcbf61183f5550e9b19
Asappayy <froshmedia@gmail.com>
2023-04-23 12:31:02 -0700

    samie-yebo

---
+++ controllers/posts.js
@@ -0,0 +1,26 @@
+import PostMessage from '../models/postMessage.js'
+import mongoose from 'mongoose';
+
+
+
+export const getPosts = async (req, res) => {
+    try {
+         const postMessages = await PostMessage.find()
+        res.status(200).json(postMessages)
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body
+    const newpost = new PostMessage(post)
+
+    try {
+        await newpost.save()
+        res.status(201).json(newpost)
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
+
+}
